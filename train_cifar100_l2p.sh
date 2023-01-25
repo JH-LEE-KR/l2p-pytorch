e351f8ead6bd4570a532a67071c6f42d2b90ab21
Jaeho Lee <dlwogh9344@khu.ac.kr>
2023-01-26 08:48:03 +0900

    Update train_cifar100_l2p.sh

--- train_cifar100_l2p.sh
+++ train_cifar100_l2p.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-gpu=4
 #SBATCH --mem-per-gpu=20G
 #SBATCH --time=8:00:00
-#SBATCH -o %x_%j.out
-#SBTACH -e %x_%j.err
+#SBATCH -o %N_%x_%j.out
+#SBTACH -e %N_%x_%j.err
 
 source /data/jaeho/init.sh
 conda activate torch38gpu
